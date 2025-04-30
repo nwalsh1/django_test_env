5cec98a5c98318dd650074737ee62096fd019293
Neil <78010987+nwalsh1@users.noreply.github.com>
2025-04-30 11:56:17 -0400

    finished learning templates

--- pages/views.py
+++ pages/views.py
@@ -7,7 +7,18 @@ def home_view(request):
     return render(request, 'home.html') #render the home.html template
 
 def contact_view(request):
+    
     return render(request, 'contact.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    my_context = {
+        'title': 'This is my first django project',
+        'my_number': 123,
+        'my_list': [145, 223, 3325, 462],
+        'my_html': '<h1> Hello World </h1>'
+    }
+
+    return render(request, 'about.html', my_context)
+
+def social_view(request,*args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
