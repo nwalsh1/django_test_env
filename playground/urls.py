d8987ed1208888300cd1b718b546ca24cf661020
Neil <78010987+nwalsh1@users.noreply.github.com>
2025-05-17 13:02:56 -0400

    Added navbar to base.html for easier navigation

--- playground/urls.py
+++ playground/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from . import views #to reference views
 
+app_name = 'playground'
 #url configurations
 urlpatterns = [
-    path('hello/',views.say_hello,), #this is the url we will use to access this view
+    path('hello/',views.say_hello,name = "hello"), #this is the url we will use to access this view
 ]
